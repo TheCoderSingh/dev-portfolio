cb4ab4d3f019cc08fa6f007aa17819b71f7a3caf
Jaskaran Singh <jaskaran1523@gmail.com>
2021-12-15 20:43:59 -0800

    Create menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,14 @@
-import { Box, Image } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
 
-import menuIcon from "../assets/hamburger.png";
+import "../styles/menu.scss";
 
 const Menu = () => {
 	return (
-		<Box className="inner">
-			<Image src={menuIcon} w={10} ml="auto" mt={4} cursor={"pointer"} />
-		</Box>
+		<Link href="#" className="menu">
+			<hr />
+			<hr />
+			<hr />
+		</Link>
 	);
 };
 
