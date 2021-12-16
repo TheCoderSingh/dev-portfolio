6d574769233d41b261d3745a542854b880fbd2dc
Jaskaran Singh <jaskaran1523@gmail.com>
2021-12-15 21:00:31 -0800

    Add height to full page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { Box, Heading } from "@chakra-ui/react";
 
 import "./App.scss";
-import Menu from "./components/Menu";
 import Home from "./pages/Home";
 
 function App() {
 	return (
 		<Box className="App">
-			<Menu />
 			<Home />
 		</Box>
 	);
