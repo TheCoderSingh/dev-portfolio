1d01a06bc58d09c8e65b86f037eaebc4613a5944
Jaskaran Singh <jaskaran1523@gmail.com>
2022-01-03 01:49:09 -0800

    Create footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
@@ -19,6 +20,7 @@ function App() {
 			<About />
 			<Skills />
 			<Contact />
+			<Footer />
 		</Box>
 	);
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import { Box, Text } from "@chakra-ui/react";
+
+import "../styles/footer.scss";
+
+const Footer = () => {
+	return (
+		<Box className="footer">
+			<Text>Designed &amp; Developed By Jaskaran Singh</Text>
+		</Box>
+	);
+};
+
+export default Footer;
