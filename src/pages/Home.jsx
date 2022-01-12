c5579a287ad372c34a4ef6157b4d93f1bcaa3be1
Jaskaran Singh <jaskaran1523@gmail.com>
2022-01-12 14:29:59 -0800

    Make home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,78 +14,152 @@ const Home = () => {
 	return (
 		<Box className="intro">
 			<Box className="inner">
-				<Box className="intro-title">
-					<Heading as="h2">Hi!</Heading>
-					<Heading as="h2">I'm Jaskaran.</Heading>
-					<Flex className="intro-title-aka">
-						<Text>a.k.a</Text>
-						<Text>&nbsp;TheCoderSingh</Text>
-					</Flex>
-				</Box>
-				<Box className="intro-description">
-					<Text>
-						I'm a <span>software developer</span>, based in
-						Vancouver, BC with a vision to revolutionize the world
-						through code.
-					</Text>
-					<Text>
-						Currently looking for opportunities. View my resume
-						<a
-							href="https://drive.google.com/file/d/1XpmxqBWO8oeApiSrrJnHCb9fDzx35ElL/view?usp=sharing"
-							target={"_blank"}
-							rel="noopener rorefferer"
+				<Box className="intro-mobtab">
+					<Box className="intro-title">
+						<Heading as="h2">Hi!</Heading>
+						<Heading as="h2">I'm Jaskaran.</Heading>
+						<Flex className="intro-title-aka">
+							<Text>a.k.a</Text>
+							<Text>&nbsp;TheCoderSingh</Text>
+						</Flex>
+					</Box>
+					<Box className="intro-description">
+						<Text>
+							I'm a <span>software developer</span>, based in
+							Vancouver, BC with a vision to revolutionize the
+							world through code.
+						</Text>
+						<Text>
+							Currently looking for opportunities. View my resume
+							<a
+								href="https://drive.google.com/file/d/1XpmxqBWO8oeApiSrrJnHCb9fDzx35ElL/view?usp=sharing"
+								target={"_blank"}
+								rel="noopener rorefferer"
+							>
+								{" "}
+								here
+							</a>
+							.
+						</Text>
+					</Box>
+					<Box className="intro-sections intro-sections-mobile">
+						<Flex className="intro-section">
+							<Box className="intro-section-image">
+								<Image src={coffee} alt="coffee mug" />
+							</Box>
+							<Box className="intro-section-content">
+								<Heading>Wanna grab a coffee?</Heading>
+								<Text>Shoot me an email at</Text>
+								<Link href="mailto:thecodersingh@gmail.com">
+									thecodersingh@gmail.com
+								</Link>
+							</Box>
+						</Flex>
+						<hr />
+						<Flex
+							className="intro-section"
+							justifyContent={"right"}
 						>
-							{" "}
-							here
-						</a>
-						.
-					</Text>
-				</Box>
-				<Box className="intro-sections">
-					<Flex className="intro-section">
-						<Box className="intro-section-image">
-							<Image src={coffee} alt="coffee mug" />
-						</Box>
-						<Box className="intro-section-content">
-							<Heading>Wanna grab a coffee?</Heading>
-							<Text>Shoot me an email at</Text>
-							<Link href="mailto:thecodersingh@gmail.com">
-								thecodersingh@gmail.com
-							</Link>
-						</Box>
+							<Box
+								className="intro-section-content"
+								textAlign={"right"}
+								mr={2}
+							>
+								<Heading>Wanna skip to the good part?</Heading>
+								<Link href="#">View my projects</Link>
+							</Box>
+							<Box className="intro-section-image">
+								<Image src={projects} alt="projects" />
+							</Box>
+						</Flex>
+					</Box>
+					<Flex
+						className="intro-sections intro-sections-tab"
+						justifyContent="space-around"
+					>
+						<Flex className="intro-section">
+							<Box className="intro-section-image">
+								<Image src={coffee} alt="coffee mug" />
+							</Box>
+							<Box className="intro-section-content">
+								<Heading>Wanna grab a coffee?</Heading>
+								<Text>Shoot me an email at</Text>
+								<Link href="mailto:thecodersingh@gmail.com">
+									thecodersingh@gmail.com
+								</Link>
+							</Box>
+						</Flex>
+						<Flex className="intro-section">
+							<Box className="intro-section-image">
+								<Image src={projects} alt="projects" />
+							</Box>
+							<Box className="intro-section-content" mr={2}>
+								<Heading>Wanna skip to the good part?</Heading>
+								<Link href="#">View my projects</Link>
+							</Box>
+						</Flex>
 					</Flex>
-					<hr />
-					<Flex className="intro-section" justifyContent={"right"}>
-						<Box
-							className="intro-section-content"
-							textAlign={"right"}
-							mr={2}
-						>
-							<Heading>Wanna skip to the good part?</Heading>
-							<Link href="#">View my projects</Link>
+					{/* <Box className="intro-scroll">
+						<Image src={scroll} alt="scroll down arrow" />
+					</Box> */}
+				</Box>
+				<Box className="intro-desktop">
+					<Box className="intro-text">
+						<Box className="intro-title">
+							<Heading as="h2">Hi!</Heading>
+							<Heading as="h2">I'm Jaskaran.</Heading>
+							<Flex className="intro-title-aka">
+								<Text>a.k.a</Text>
+								<Text>&nbsp;TheCoderSingh</Text>
+							</Flex>
 						</Box>
-						<Box className="intro-section-image">
-							<Image src={projects} alt="projects" />
+						<Box className="intro-description">
+							<Text>
+								I'm a <span>software developer</span>, based in
+								Vancouver, BC with a vision to revolutionize the
+								world through code.
+							</Text>
+							<Text>
+								Currently looking for opportunities. View my
+								resume
+								<a
+									href="https://drive.google.com/file/d/1XpmxqBWO8oeApiSrrJnHCb9fDzx35ElL/view?usp=sharing"
+									target={"_blank"}
+									rel="noopener rorefferer"
+								>
+									{" "}
+									here
+								</a>
+								.
+							</Text>
 						</Box>
+					</Box>
+					<Box className="separator" />
+					<Flex className="intro-sec">
+						<Flex className="intro-section">
+							<Box className="intro-section-image">
+								<Image src={coffee} alt="coffee mug" />
+							</Box>
+							<Box className="intro-section-content">
+								<Heading>Wanna grab a coffee?</Heading>
+								<Text>Shoot me an email at</Text>
+								<Link href="mailto:thecodersingh@gmail.com">
+									thecodersingh@gmail.com
+								</Link>
+							</Box>
+						</Flex>
+
+						<Flex className="intro-section">
+							<Box className="intro-section-image">
+								<Image src={projects} alt="projects" />
+							</Box>
+							<Box className="intro-section-content" mr={2}>
+								<Heading>Wanna skip to the good part?</Heading>
+								<Link href="#">View my projects</Link>
+							</Box>
+						</Flex>
 					</Flex>
 				</Box>
-				{/* <Flex className="intro-social">
-					<Link href="#">
-						<Image src={facebook} alt="facebook logo" />
-					</Link>
-					<Link href="#">
-						<Image src={instagram} alt="instagram logo" />
-					</Link>
-					<Link href="#">
-						<Image src={twitter} alt="twitter logo" />
-					</Link>
-					<Link href="#">
-						<Image src={github} alt="github logo" />
-					</Link>
-					<Link href="#">
-						<Image src={linkedin} alt="linkedin logo" />
-					</Link>
-				</Flex> */}
 				<Box className="intro-scroll">
 					<Image src={scroll} alt="scroll down arrow" />
 				</Box>
